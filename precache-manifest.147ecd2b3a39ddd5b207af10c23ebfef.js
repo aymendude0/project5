6321244c4bc23700924ab137476c665de9d9ef78
Aymen Mahmod <aymenm@live.com>
2020-08-10 16:41:53 -0400

    Updates

---
+++ precache-manifest.147ecd2b3a39ddd5b207af10c23ebfef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd15a9d06d35b8a2b16a4586046c75c",
+    "url": "/project5/index.html"
+  },
+  {
+    "revision": "721f79acf20363a0ef24",
+    "url": "/project5/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c4a04c22a2e116987ab",
+    "url": "/project5/static/js/2.b86c05ed.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/project5/static/js/2.b86c05ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "721f79acf20363a0ef24",
+    "url": "/project5/static/js/main.220bb1dc.chunk.js"
+  },
+  {
+    "revision": "392a38de9e3f2105cc72",
+    "url": "/project5/static/js/runtime-main.99ddbd30.js"
+  }
+]);
